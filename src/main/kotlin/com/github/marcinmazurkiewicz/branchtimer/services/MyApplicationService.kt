43499ac58dca6bfd44828dbe70ffa9b837a43075
GitHub Action <action@github.com>
2022-06-30 18:22:15 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/marcinmazurkiewicz/branchtimer/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.marcinmazurkiewicz.branchtimer.services
+
+import com.github.marcinmazurkiewicz.branchtimer.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/marcinmazurkiewicz/branchtimer/services/MyProjectService.kt
+++ src/main/kotlin/com/github/marcinmazurkiewicz/branchtimer/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.marcinmazurkiewicz.branchtimer.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.marcinmazurkiewicz.branchtimer.MyBundle
 
 class MyProjectService(project: Project) {
 
